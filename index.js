bd7ab9a91ab683ccb34dda42836eed8cecae51d0
kapilpratihar <109463318+kapilpratihar@users.noreply.github.com>
2022-07-17 16:38:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const {Server} = require('socket.io');
+const path =require('path');
+const express=require("express");
+const app=express();
+const http=require("http");
+const server=http.createServer(app);
+const io=new Server(server);
+console.log("hii");
+app.get("/",(req,res)=>{
+    console.log("hii2");
+    app.use(express.static(path.join(__dirname,"./public")));
+    res.sendFile(__dirname+"/public/index.html");
+})
+const users = {};
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+        // console.log("New user",name);
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined',name);
+    });
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect',message =>{
+      socket.broadcast.emit('left',users[socket.id]);
+      delete users[socket.id];
+});
+
+})
+server.listen(3000);
+console.log("app is running at port 3000");
