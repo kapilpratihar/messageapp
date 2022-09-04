95368e91795802f6696bc772992502d48860f719
kapilpratihar <109463318+kapilpratihar@users.noreply.github.com>
2022-09-04 15:46:14 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -35,3 +35,13 @@ document.getElementById('submit').addEventListener("click",()=>{
     socket.emit('send',message);
     messageInput.value = ''
 })
+
+
+// function submitOnEnter(event){
+//     if(event.which === 13){
+//         event.target.form.dispatchEvent(new Event("submit", {cancelable: true}));
+//         event.preventDefault(); // Prevents the addition of a new line in the text field (not needed in a lot of cases)
+//     }
+// }
+
+// document.getElementById("usermsg").addEventListener("keypress", submitOnEnter);
